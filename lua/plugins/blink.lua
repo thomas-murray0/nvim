4fb26351c64669f35ed261c22822139c32fbe458
Thomas Murray <tmurray2123@gmail.com>
2025-10-17 15:57:52 -0400

    configuring blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,24 +1,48 @@
 vim.pack.add({
 	{ src = 'https://github.com/saghen/blink.cmp', version = "1.*" },
 })
+
 require("blink.cmp").setup(
 	{
 		signature = { enabled = true },
 		keymap = {
-			preset = 'default',
+			preset = 'none',
+			['<C-k>'] = { 'show', 'show_documentation', 'hide_documentation', 'fallback' },
+			['<C-e>'] = { 'hide', 'fallback' },
+			['<C-y>'] = { 'select_and_accept', 'fallback' },
+
+			['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+			['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+			['Up'] = { 'scroll_documentation_up', 'fallback' },
+			['<Down>'] = { 'scroll_documentation_down', 'fallback' },
+
+			['<C-q>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end }, -- show snippets
+
+			['<Tab>'] = { 'snippet_forward', 'fallback' },
+			['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+			['<C-a>'] = { 'show_signature', 'hide_signature', 'fallback' },
 		},
 		appearance = {
-			nerd_font_variant = 'mono'
+			nerd_font_variant = 'mono',
+			kind_icons = {
+				Snippet = '' -- cause why not
+			},
 		},
 		sources = {
 			default = { 'lsp', 'path', 'buffer', 'snippets' },
 		},
 		snippets = { preset = 'luasnip' },
 		completion = {
+			-- ghost_text = { enabled = true },
 			documentation = { auto_show = true },
 			menu = {
 				auto_show = true,
-				draw = { treesitter = { "lsp" } }
+				draw = {
+					treesitter = { "lsp" },
+					columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
+				},
 			},
 		},
 		fuzzy = { implementation = "prefer_rust_with_warning" },
