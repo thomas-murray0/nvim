a0b9f2c9bda4ef3a63e004331c5f98ee4d0fe68e
Thomas Murray <tmurray2123@gmail.com>
2025-10-22 15:59:49 -0400

    added more colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,16 @@
 vim.pack.add({
-	{ src = 'https://github.com/catppuccin/nvim',    name = 'catppuccin' },
-	{ src = 'https://github.com/kuri-sun/yoda.nvim', name = 'yoda' },
+	{ src = 'https://github.com/catppuccin/nvim',              name = 'catppuccin' },
+	{ src = 'https://github.com/kuri-sun/yoda.nvim',           name = 'yoda' },
+	{ src = 'https://github.com/ellisonleao/gruvbox.nvim',     name = 'gruvbox' },
+	{ src = 'https://github.com/folke/tokyonight.nvim',        name = 'tokyo-night' },
+	{ src = 'https://github.com/dgox16/oldworld.nvim',         name = 'oldworld' },
+	{ src = 'https://github.com/Mofiqul/dracula.nvim',         name = 'dracula' },
+	{ src = 'https://github.com/xero/miasma.nvim',             name = 'miasma' },
+	{ src = 'https://github.com/rebelot/kanagawa.nvim',        name = 'kanagawa' },
+	{ src = 'https://github.com/Mofiqul/vscode.nvim',          name = 'vscode' },
+	{ src = 'https://github.com/shaunsingh/nord.nvim',         name = 'nord' },
+	{ src = 'https://github.com/eldritch-theme/eldritch.nvim', name = 'eldritch' },
+	{ src = 'https://github.com/rose-pine/neovim',             name = 'rose-pine' },
 })
 
 -- sets colorscheme
