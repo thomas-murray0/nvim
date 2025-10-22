077a18b8bfe321a5b4f8c1ad3d54590f48449e00
Thomas Murray <tmurray2123@gmail.com>
2025-10-22 13:36:53 -0400

    changed colors again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
 	{ src = 'https://github.com/catppuccin/nvim',    name = 'catppuccin' },
-	{ src = 'https://github.com/kuri-sun/yoda.nvim', 'yoda' },
+	{ src = 'https://github.com/kuri-sun/yoda.nvim', name = 'yoda' },
 })
 
 -- sets colorscheme
-vim.cmd('colorscheme yoda')
+vim.cmd('colorscheme catppuccin')
