e571c5ad35217217c8b2708b4225410d51b78496
Thomas Murray <tmurray2123@gmail.com>
2026-01-06 18:34:03 -0500

    added zig file snippets

--- lua/snippets/zig.lua
+++ lua/snippets/zig.lua
@@ -20,6 +20,37 @@ return {
 			defer _ = gpa.deinit();
 			const allocator = gpa.allocator();
 			]], {})),
+  s("rfile_alloc", fmt([[
+		var file: std.fs.File = try std.fs.cwd().openFile({}, .{{ .mode = {}}});
+		defer file.close();
+		var file_reader: std.fs.File.Reader = file.reader(&.{{}});
+		const file_interface: *std.Io.Reader = &file_reader.interface;
+		var buffer: []u8 = try file_interface.allocRemaining(allocator, .unlimited);
+  ]], {
+    i(1, "path"),
+    i(2, ".read_only")
+  })),
+  s("rfile_buffer", fmt([[
+		var file: std.fs.File = try std.fs.cwd().openFile({}, .{{ .mode = {}}});
+		defer file.close();
+		var buffer_reader: [{}]u8 = undefined;
+		var file_reader: std.fs.File.Reader = file.reader(&buffer_reader);
+  ]], {
+    i(1, "path"),
+    i(2, ".read_only"),
+    i(3, "1024")
+  })),
+  s("wfile_buffer", fmt([[
+		var file: std.fs.File = try std.fs.cwd().openFile({}, .{{ .mode = {}}});
+		defer file.close();
+		var buffer_writer: [{}]u8 = undefined;
+		var file_writer: std.fs.File.Writer = file.writer(&buffer_writer);
+		const file_interface: *std.Io.Writer = &file_writer.interface;
+  ]], {
+    i(1, "path"),
+    i(2, ".write_only"),
+    i(3, "1024")
+  })),
 	s('dprint', {
 		t 'std.debug.print("', i(1), t '", .{', i(2), t '});', i(0),
 	}),
