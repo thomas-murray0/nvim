4c2b9c635bfbdf4f0f492c29beac4f13b88e6d7d
Thomas Murray <tmurray2123@gmail.com>
2026-01-10 15:24:24 -0500

    added snippets for all files

---
+++ lua/snippets/all.lua
@@ -0,0 +1,29 @@
+-- All snippet things
+-- local ls = require("luasnip")
+-- local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local t = ls.text_node
+-- local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+--
+-- local ls_ex = require("luasnip.extras")
+-- local l = ls_ex.lambda
+-- local rep = ls_ex.rep
+-- local p = ls_ex.partial
+-- local m = ls_ex.match
+-- local n = ls_ex.nonempty
+-- local dl = ls_ex.dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local types = require("luasnip.util.types")
+-- local conds = require("luasnip.extras.conditions")
+-- local conds_expand = require("luasnip.extras.conditions.expand")
+-- look up teej video on fmta -> 'https://www.youtube.com/watch?v=aNWx-ym7jjI'
+
+return {
+s("qwerty", t("qwertyuiopasdfghjklzxcvbnm")),
+s("abc", t("abcdefghijklmnopqrstuvwxyz")),
+}
